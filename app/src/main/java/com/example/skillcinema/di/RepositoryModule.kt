f9ab36d440a4353f96bbbfba14af18c09922f64e
Марков Алексей <aleks-mark-dzr@yandex.ru>
2025-01-11 20:36:06 +0300

    final_android_lvl1-setting hilt

--- app/src/main/java/com/example/skillcinema/di/RepositoryModule.kt
+++ app/src/main/java/com/example/skillcinema/di/RepositoryModule.kt
@@ -7,13 +7,26 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+object NetworkModule {
+
     @Provides
     @Singleton
-    fun provideMovieRepository(apiService: MovieApiService): MovieRepository =
-        MovieRepositoryImpl(apiService)
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://kinopoiskapiunofficial.tech")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieApiService(retrofit: Retrofit): MovieApiService {
+        return retrofit.create(MovieApiService::class.java)
+    }
 }
--- app/src/main/java/com/example/skillcinema/network/MovieApiService.kt
+++ app/src/main/java/com/example/skillcinema/network/MovieApiService.kt
@@ -17,14 +17,14 @@ interface MovieApiService {
     }
 }
 
-object RetrofitInstance {
-    private const val BASE_URL = "https://kinopoiskapiunofficial.tech"
-
-    val movieApiService: MovieApiService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(MovieApiService::class.java)
-    }
-}
+//object RetrofitInstance {
+//    private const val BASE_URL = "https://kinopoiskapiunofficial.tech"
+//
+//    val movieApiService: MovieApiService by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(MovieApiService::class.java)
+//    }
+//}
