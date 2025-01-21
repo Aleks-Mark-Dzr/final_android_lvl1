ab72927c903840a94112c104713f5a3030505ea4
Марков Алексей <aleks-mark-dzr@yandex.ru>
2025-01-21 13:48:27 +0300

    final_android_lvl1_modify code without hilt 2

--- app/src/main/java/com/example/skillcinema/network/MovieApiService.kt
+++ app/src/main/java/com/example/skillcinema/network/MovieApiService.kt
@@ -11,8 +11,4 @@ interface MovieApiService {
 //    @Headers("X-API-KEY: $API_KEY")
     @GET("/api/v2.2/films/top?type=TOP_250_BEST_FILMS")
     suspend fun getTopMovies(@Query("page") page: Int): TopMoviesResponse
-
-    companion object {
-        private const val API_KEY = "f08ba7c2-5719-4c08-9888-764c3e4954f5"
-    }
 }
